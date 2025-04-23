802543240c6e43877068de334d6ba880cff808ed
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2025-04-23 14:02:42 +0200

    Fix: error.text() is not always defined

--- src/request.js
+++ src/request.js
@@ -27,8 +27,12 @@ class Request {
       const response = await action(Request.path(route), payload);
       return response;
     } catch (error) {
-      const errorMessage = await error.text();
-      throw new Error(errorMessage);
+      if (error && typeof error.text === 'function') {
+        const errorMessage = await error.text();
+        throw new Error(errorMessage);
+      }
+
+      throw new Error(error?.message || 'An unknown error occurred');
     }
   }
 
